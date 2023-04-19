7f373f5c4fd6531921b1f1fc575bc193746c33b8
hoangtuan <hoangtuan.work04@gmail.com>
2023-04-20 00:13:30 +0700

    Models

--- website/models.py
+++ website/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Record(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    first_Name = models.CharField(max_length=50)
+    last_Name = models.CharField(max_length=50)
+    email = models.EmailField()
+    phone = models.CharField(max_length=10)
+
+    
+    def __str__(self):
+        return(f"{self.first_Name} {self.last_Name}")
--- website/views.py
+++ website/views.py
@@ -2,10 +2,14 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .form import SignUpForm
+from .models import Record
 
 
 
 def home(request):
+    records = Record.objects.all()
+
+
     if request.method == "POST":
         username = request.POST['username']
         password = request.POST['password']
@@ -19,7 +23,7 @@ def home(request):
             messages.success(request, 'Oops, cant login, try again')
             return redirect('home')
     else:
-        return render(request, 'home.html', {})
+        return render(request, 'home.html', {'records':records})
 
     return render(request, 'home.html', {})
 
@@ -32,4 +36,19 @@ def logout_user(request):
     return redirect('home')
 
 def register_user(request):
-    return render(request, 'register.html', {})
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username = username, password = password)
+            login(request, user)
+            messages.success(request, 'Hello helu helo heli')
+            return redirect('home')
+    else:
+        form = SignUpForm()
+        return render(request, 'register.html', {'form':form})
+    
+    return render(request, 'register.html', {'form':form})
